bf1a6accd3f6b5a7ccbcfdfa81986e5d47f6dd05
andreealalaaaa <73436585+andreealalaaaa@users.noreply.github.com>
2023-05-02 12:05:24 +0300

    Initial Commit

---
+++ Indoor-Navigation/Indoor_NavigationApp.swift
@@ -0,0 +1,20 @@
+//
+//  Indoor_NavigationApp.swift
+//  Indoor-Navigation
+//
+//  Created by Andreea Miculescu on 02.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Indoor_NavigationApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
